a3f6978271c9926951c061302ddb06fdd7730270
Admin <admin@MacBook-Pro-de-Admin.local>
2021-07-09 12:39:16 -0500

    Cambios menores

--- 202012_ProvinciaCartama.R
+++ 202012_ProvinciaCartama.R
@@ -15,9 +15,21 @@ library(scales)
 
 #### Importamos los datos ####
 # ECV Departamental:
-Datos <- read_excel("/Volumes/Respaldo/Google Drive (lehyton.arenas@ucn.cl)-Actualizado/Lehyton Windows/Empleo/Ecsim/2020_Proyecto Centralidad Cartama/Entregables_Lehyton/Datos.xlsx")
+Datos <- read_excel("/Volumes/Respaldo/Google Drive (lehyton.arenas@ucn.cl)-Actualizado/Lehyton Windows/Empleo/Ecsim/2020_Proyecto Centralidad Cartama/Entregables_Lehyton/Datos_V3.xlsx")
 # Eliminamos decimales:
-Datos[,3:33] <- round(Datos[,3:33],2)
+Datos[,2:26] <- round(Datos[,2:26],2)
+# Ponemos los municipios en orden:
+Datos$Municipio <- factor(Datos$Municipio, levels=c("Departamento de Antioquia",
+                                                    "Valle de Aburrá",
+                                                    "Medellín",
+                                                    "Provincia del Cartama",
+                                                    "Caramanta",
+                                                    "Concordia",
+                                                    "Fredonia","Hispania",
+                                                    "Jericó","La Pintada",
+                                                    "Pueblorrico","Salgar",
+                                                    "Támesis","Tarso",
+                                                    "Valparaíso","Venecia"))
 
 # PIB Per cápita:
 Economia <- read_excel("/Volumes/Respaldo/Google Drive (lehyton.arenas@ucn.cl)-Actualizado/Lehyton Windows/Empleo/Ecsim/2020_Proyecto Centralidad Cartama/Entregables_Lehyton/PIB_Municipios.xlsx", sheet = "PIB_Percapita")
@@ -36,47 +48,33 @@ tiff(filename = "Grafico_CRContributivo.tiff", # Cambiar indicador a gusto
      width = 800, height = 600)
 
 ggplot(data=Datos,
-       aes(y=Municipio, x=Ingresos))+ # Cambiar indicador a gusto
-  geom_bar(stat="identity", position="dodge", fill="cyan4")+
-  ylab("Municipios")+
-  xlab("Ingresos población ocupada (miles)")+ # Cambiar indicador a gusto
-  geom_text(aes(label = Ingresos), # Cambiar indicador a gusto
+       aes(y=Municipio, x=IDE))+ # Cambiar indicador a gusto
+  geom_bar(stat="identity", position="dodge", fill="aquamarine3")+
+  ylab("Localización")+
+  xlab("Índice de Dependencia Económica")+ # Cambiar indicador a gusto
+  geom_text(aes(label = IDE), # Cambiar indicador a gusto
             size=4, vjust=0.5, hjust=0 ,col="black")+
   theme(axis.title=element_text(size=rel(1.3)),
          axis.text.x = element_text(size=rel(1.3)),
          axis.text.y = element_text(size=rel(1.3)))+
-  scale_x_continuous(limits=c(0,1060))
+  scale_x_continuous(limits=c(0,67))
 
 dev.off()
 
-tiff(filename = "Grafico_PIBPC.tiff", # Cambiar indicador a gusto
-     width = 800, height = 600)
-ggplot(data=Economia,
-       aes(y=Municipio, x=PIB_PC))+ # Cambiar indicador a gusto
-  geom_bar(stat="identity", position="dodge", fill="dodgerblue2")+
-  ylab("Municipios")+
-  xlab("PIB per cápita (miles de pesos)")+ # Cambiar indicador a gusto
-  geom_text(aes(label = PIB_PC), # Cambiar indicador a gusto
-            size=4, vjust=0.5, hjust=0 ,col="black")+
-  theme (axis.text.x = element_text(size=rel(1.5)),
-         axis.text.y = element_text(size=rel(1.5)))
-dev.off()
 
 #### Gráficos de +2  vbles ####
 # Se hacen en 3 pasos:
 #1. Aplicamos filtros por Polo e indicadores
 DosVbles <- Datos %>% 
-  dplyr::select(.,c(Municipio,TNE5,TNE6_10,TNE11_14,TNE15_16,TNE17_21)) %>%  # cambiar indicadores
-  dplyr::rename("TNE 5 años"=TNE5,"TNE 6-10 años"=TNE6_10,
-                "TNE 11-14 años"=TNE11_14,"TNE 15-16 años"=TNE15_16,
-                "TNE 17-21 años"=TNE17_21) # cambiar indicadores
+  dplyr::select(.,c(Municipio,`Deporte-Recreación`,EventosCulturales)) %>%  # cambiar indicadores
+  dplyr::rename("Deporte y Recreación"=`Deporte-Recreación`,"Cultura"=EventosCulturales) # cambiar indicadores
 
 #2. Transformamos la data a tipo Long
 Data_long <- DosVbles %>% 
-  gather("Indicador:", "Valor", -Municipio)
+  gather("Indicador", "Valor", -Municipio)
 
 # Paso opcional: poner data en factores para un orden establecido:
-Data_long$Indicador <- factor(Data_long$`Indicador:`, levels=c("TNE 5 años","TNE 6-10 años","TNE 11-14 años","TNE 15-16 años","TNE 17-21 años"))
+#Data_long$Indicador <- factor(Data_long$`Indicador:`, levels=c("TNE 5 años","TNE 6-10 años","TNE 11-14 años","TNE 15-16 años","TNE 17-21 años"))
 
 # 3A. Grafico +2 variables, tipo 3A: Con dodge: columna a lado y lado
 tiff(filename = "Grafico_TNE.tiff", # Cambiar indicador a gusto
@@ -84,18 +82,19 @@ tiff(filename = "Grafico_TNE.tiff", # Cambiar indicador a gusto
 
 ggplot(Data_long, aes(x = Valor, y = Municipio, fill = Indicador))+
   geom_bar(stat="identity",position="dodge")+
-  labs(x="Indicadores: Tasa Neta Escolaridad (TNE)",y="Municipios",fill="Indicador:")+
+  labs(x="Valor indicador",y="Localización",fill="Indicador:")+
   geom_text(aes(label = Valor),position = position_dodge(1),
             size=4.4, vjust=0.4, hjust=-0.05 ,col="black")+
   theme(axis.title=element_text(size=rel(1.5)),
         axis.text.x=element_text(size=rel(1.5)),
         axis.text.y=element_text(size=rel(1.5)),
         legend.title=element_text(size=rel(1.3)),
-        legend.text=element_text(size=rel(1)),
-        legend.position="top"
+        legend.text=element_text(size=rel(1.1)),
+        legend.position="top",
+        legend.direction = "vertical"
         )+
-  scale_x_continuous(limits=c(0,100))+
-  scale_fill_brewer(palette=1)
+  scale_x_continuous(limits=c(0,35))#+
+  #scale_fill_brewer(palette=1)
 
 dev.off() 
 
